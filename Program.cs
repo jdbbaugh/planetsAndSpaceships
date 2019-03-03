b8d4464817d189f25a66e0754ea59e4a02d5e190
jdbbaugh <jbaugh78@msn.com>
2019-03-03 15:34:02 -0600

    completed practice

--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace planetsAndSpaceships
     {
         public static void Main()
         {
+            List<string> planetList = new List<string>() {"Mercury", "Mars"};
 
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> morePlanets = new List<string>() {"Uranus","Neptune"};
+            planetList.AddRange(morePlanets);
+
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+            List<string> rockyPlanets = new List<string>(planetList.GetRange(0, 4));
+            planetList.Remove("Pluto");
+
+            foreach(string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("rocky planets");
+            foreach(string planet in rockyPlanets)
+            {
+                Console.WriteLine(planet);
+            }
         }
     }
 }
